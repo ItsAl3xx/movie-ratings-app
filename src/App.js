2d1f7bbc6b86c8cbecba76e9d7cf0bec970c2223
ItsAl3xx <alexbrav0@icloud.com>
2024-04-12 12:53:59 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-// App.js
+// src/App.js
 import React from 'react';
-import './App.css'; // This imports the CSS from App.css
-import LoginForm from './components/LoginForm'; // This imports the LoginForm component
-import Navigation from './components/Navigation'; // This imports the Navigation component
+// Import your components here
 
-// This is the main App component that will render our components on the screen
 function App() {
   return (
     <div className="App">
-      <Navigation /> {/* This renders the Navigation component */}
-      <div className="form-container"> {/* This div will center the LoginForm */}
-        <LoginForm /> {/* This renders the LoginForm component */}
-      </div>
+      {/* Your components will be rendered here */}
     </div>
   );
 }
 
-export default App; 
+export default App;
